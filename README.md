c57b812cbaef96f2ab7561e1fdb81123a2507cec
Mgamboa <marianellag400@gmail.com>
2022-10-02 20:15:48 -0500

    readme was added, along with project screenshots

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# Note-Taker
+# Note-Taker link:
+
+
+# **Note Taker**
+
+[![Licence](http://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+---
+
+
+# **Table of Contents**
+
+-[Installation Instructions](#installation)
+-[Usage Information](#usage-information)
+-[License](#license)
+-[Contributing Guidelines](#contribution-guidelines)
+-[Test Instructions](#test-instructions)
+-[Question](#questions)
+
+
+---
+
+## **Description of the Project**
+A quick note reminder that can take in multiple notes. User can view notes and what they contain, as well as delete no longer needed notes.
+
+
+
+## **Installation**
+User can click on link provided in README to see Note Taker.
+
+
+
+## **Usage Information**
+When user needs to write a note, user can enter a title and quick text and save note. Multiple notes can be added if needed. Once user doesn't need or want a specific note, user can simply click on delete and note will be removed.
+
+
+
+## **Contribution Guidelines**
+Anyone can contribute to the project, as long as the it is agreed upon by the author, in order to merge any confirmed changes.
+
+
+
+## **Test Instructions**
+User can clone copy of project and test project. If user believes that the project lacks something and would like to contribute, please see contribution guidelines.
+
+
+
+## **Questions**
+### If you have any questions about the project, please contact me at: mailto:marianellag400@gmail.com. 
+### You can also see my projects and works at: https://github.com/Marianellag1
+
+
+
+### **License**
+This project is licensed under:
+    MIT
+
+
--- server.js
+++ server.js
@@ -1,50 +1,46 @@
 //  all requires
 const express = require('express');
 const path = require('path');
-const notes = require('./db/db.json');
 const fs = require('fs');
 
-//`app` variable set to the value of `express()`.**
+//`app` variable set to the value of `express()`.
 const app = express();
 const PORT = process.env.port || 3001;
 
 app.use(express.json());//my middleware
 app.use(express.urlencoded({ extended: true }));//taken from notes
-//Invoke app.use() and serve static files from the '/public' folder**
+//Invoke app.use() and serve static files from the '/public' folder
 app.use(express.static('public'));
-
-// The following HTML routes should be created:
-
-// * `GET /notes` should return the `notes.html` file.
-
-// * `GET *` should return the `index.html` file.
-
-// The following API routes should be created:
-
-// * `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
-
-// * `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
-
+//* `GET *` should return the `index.html` file.
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 
 //route that will serve up the `public/notes.html` page
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
 
-
+// `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
 app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, "./db/db.json")));
 
-app.post('/api/notes', (req, res) => {
+//* `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, 
+//and then return the new note to the client. 
+app.post('/api/notes', (req, res) => {//new notes can be made
   let newNote = req.body;
   let noteList = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
   let noteLength = (noteList.length).toString();
   newNote.id = noteLength;
-  noteList.push(newNote);
+  noteList.push(newNote);//pushing it to notes
   fs.writeFileSync('./db/db.json', JSON.stringify(noteList));
   res.json(noteList);
 })
 
+app.delete('/api/notes/:id', (req, res) => {//deletes notes
+  let noteList = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
+  let noteId = (req.params.id).toString();
+  noteList = noteList.filter(selected => {
+    return selected.id != noteId;
+  })
+  fs.writeFileSync('./db/db.json', JSON.stringify(noteList));
+  res.json(noteList)
+}
+);
 
 app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT} 🚀`));
-
-
-//from notes **
